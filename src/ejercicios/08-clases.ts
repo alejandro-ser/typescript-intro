fafed53dac01078e2a5d854536c1acdc44565a47
alejandro-giraldo <alejosegi@gmail.com>
2022-04-21 16:28:55 +0200

    Clases

---
+++ src/ejercicios/08-clases.ts
@@ -0,0 +1,29 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+class PersonaNormal {
+
+    constructor(
+        public nombre: string,
+        public direccion: string
+    ) {}
+}
+
+class Heroe extends PersonaNormal {
+    // alterEgo: string;
+    // edad: number;
+    // nombreReal: string;
+
+    constructor(
+        public alterEgo: string,
+        public edad?: number,
+        public nombreReal?: string
+    ) {
+        super( nombreReal, 'New York, USA' );
+    }
+}
+
+const ironman = new Heroe('Ironman', 45, 'Tony');
+
+console.log(ironman);
