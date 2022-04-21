1a95ae886127483a4414374a2701b2a5a4250f07
alejandro-giraldo <alejosegi@gmail.com>
2022-04-21 16:28:26 +0200

    Import export

---
+++ src/ejercicios/07-import-export.ts
@@ -0,0 +1,22 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+import { Producto, calculaISV } from "./06-desestructuracion-funcion";
+
+
+const carritoCompras: Producto[] = [
+    {
+        desc: 'Telefono 1',
+        precio: 100
+    },
+    {
+        desc: 'Telefono 2',
+        precio: 1050
+    },
+];
+
+const [ total, isv ] = calculaISV( carritoCompras );
+
+console.log('Total: ', total);
+console.log('ISV: ', isv);
